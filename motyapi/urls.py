15c8aaf588e6684e9a80c5db76f38724c1d0fb78
Huan Zhang <jasonzhang01@gmail.com>
2020-05-01 07:57:10 -0400

    added translate api

--- motyapi/urls.py
+++ motyapi/urls.py
@@ -5,4 +5,5 @@ from motyapi import views
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
     url(r'^get_bot_responses/$', views.BotResponseView.get_bot_responses),
+    url(r'^translate_to_chinese/$', views.BotResponseView.translate_to_chinese),
 ]
--- motyapi/views.py
+++ motyapi/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import boto3
+import json
 
 from motyapp.models import BotResponse
-  
+
+
+REGION = 'us-west-2'
+SRC_LANG = 'auto'
+TRG_LANG_ZH = 'zh'
+TRG_LANG_EN = 'en'
+
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
@@ -16,8 +24,26 @@ class BotResponseView:
             responses = BotResponse.objects.all();
             result_json = {};
             result_json['responses'] = [];
-            for response in responses:
-                result_json['responses'].append(response.to_json())
+            for response in responses:  
+                result_json['responses'].append(response.to_json());
+        except Exception as e:
+            print (e);
+            return JsonResponse({'status':'false','message':'Getting bot responses failed.'}, status=400);
+        return JsonResponse(result_json, status=200);
+    
+    @csrf_exempt
+    def translate_to_chinese(request):
+        try:
+            post_data = json.loads(request.body);
+            original_text = post_data['original_text'];
+            translate = boto3.client(service_name='translate', region_name=REGION, use_ssl=True);
+            response = translate.translate_text(Text=original_text, SourceLanguageCode=SRC_LANG, TargetLanguageCode=TRG_LANG_ZH);
+            if response['SourceLanguageCode'] == TRG_LANG_ZH:
+                response = translate.translate_text(Text=original_text, SourceLanguageCode=SRC_LANG, TargetLanguageCode=TRG_LANG_EN);
+            result = response.get('TranslatedText')
+            result_json = {};
+            result_json["origin_text"] = original_text;
+            result_json["translated_text"] = result;
         except Exception as e:
             print (e)
             return JsonResponse({'status':'false','message':'Getting bot responses failed.'}, status=400)
