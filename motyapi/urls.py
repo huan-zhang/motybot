1161647e549eef7b11f43f21235c8ed7a03c23fc
Huan Zhang <jasonzhang01@gmail.com>
2020-05-03 14:48:33 -0400

    move process request from gs to here

--- motyapi/urls.py
+++ motyapi/urls.py
@@ -4,6 +4,7 @@ from motyapi import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
-    url(r'^get_bot_responses/$', views.BotResponseView.get_bot_responses),
-    url(r'^translate_to_chinese/$', views.BotResponseView.translate_to_chinese),
+    url(r'^get_bot_responses/$', views.BotResponseView.get_bot_response_list),
+    url(r'^translate_to_chinese/$', views.BotResponseView.translate),
+    url(r'^process_bot_request/$', views.BotResponseView.process_bot_request),
 ]
--- motyapi/views.py
+++ motyapi/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import boto3
 import json
+import requests
 
 from motyapp.models import BotResponse
 
@@ -12,14 +13,31 @@ REGION = 'us-west-2'
 SRC_LANG = 'auto'
 TRG_LANG_ZH = 'zh'
 TRG_LANG_EN = 'en'
+BOT_ENDPOINT = 'https://api.telegram.org/bot1109107118:AAFLl7EOefQ17p7DIz0aFeUH1vrao0ptkNI/'
 
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
 
 class BotResponseView:
+   
+    @staticmethod
     @csrf_exempt
-    def get_bot_responses(request):
+    def get_bot_response(test_str):
+        result_str = "No response found.";
+        try:
+            responses = BotResponse.objects.all();
+            for response in responses:
+                if test_str == response.search_str:
+                    result_str = response.response_str;
+        except Exception as e:
+            print (e);
+            pass;
+        return result_str;
+    
+    @staticmethod
+    @csrf_exempt
+    def get_bot_response_list(request):
         try:
             responses = BotResponse.objects.all();
             result_json = {};
@@ -31,16 +49,29 @@ class BotResponseView:
             return JsonResponse({'status':'false','message':'Getting bot responses failed.'}, status=400);
         return JsonResponse(result_json, status=200);
     
+    @staticmethod
     @csrf_exempt
-    def translate_to_chinese(request):
+    def translate_to_chinese(original_str):
+        result_str = '';
+        try:
+            translate = boto3.client(service_name='translate', region_name=REGION, use_ssl=True);
+            response = translate.translate_text(Text=original_str, SourceLanguageCode=SRC_LANG, TargetLanguageCode=TRG_LANG_ZH);
+            if response['SourceLanguageCode'] == TRG_LANG_ZH:
+                response = translate.translate_text(Text=original_str, SourceLanguageCode=SRC_LANG, TargetLanguageCode=TRG_LANG_EN);
+            result_str = response.get('TranslatedText')
+        except Exception as e:
+            print (e);
+            pass;
+        return result_str;    
+        
+    @staticmethod
+    @csrf_exempt
+    def translate(request):
         try:
             post_data = json.loads(request.body);
+            print(post_data);
             original_text = post_data['original_text'];
-            translate = boto3.client(service_name='translate', region_name=REGION, use_ssl=True);
-            response = translate.translate_text(Text=original_text, SourceLanguageCode=SRC_LANG, TargetLanguageCode=TRG_LANG_ZH);
-            if response['SourceLanguageCode'] == TRG_LANG_ZH:
-                response = translate.translate_text(Text=original_text, SourceLanguageCode=SRC_LANG, TargetLanguageCode=TRG_LANG_EN);
-            result = response.get('TranslatedText')
+            result = BotResponseView.translate_to_chinese(original_text);
             result_json = {};
             result_json["origin_text"] = original_text;
             result_json["translated_text"] = result;
@@ -48,3 +79,80 @@ class BotResponseView:
             print (e)
             return JsonResponse({'status':'false','message':'Getting bot responses failed.'}, status=400)
         return JsonResponse(result_json, status=200)
+
+    @staticmethod
+    @csrf_exempt
+    def process_bot_request(request):
+        try:
+            post_data = json.loads(request.body);
+            print(post_data);
+            payload = BotResponseView.prepare_payload(post_data);
+            """
+            payload = {
+                "method": "sendMessage",
+                "chat_id": "" + postData['message']['chat']['id'],
+                "text": resultText,
+            };
+            """
+            resp = requests.post(BOT_ENDPOINT, payload);
+            if (resp.ok):
+                print(resp.text)
+                result_json = json.loads(resp.text);
+            else:
+                return JsonResponse({'status':'false','message':'Getting bot responses failed.'}, status=400)
+            
+        except Exception as e:
+            print (e)
+            return JsonResponse({'status':'false','message':'Getting bot responses failed.'}, status=400)
+        return JsonResponse(result_json, status=200, safe=False)
+    
+    @staticmethod
+    def prepare_payload(data):
+        try:
+            message = data['message'];
+            result_text = "hey, " + message['from']['first_name'] + " " + message['from']['last_name'];
+            
+            if 'text' in message:
+                msg_txt = message['text'].upper();
+                print (msg_txt);
+                if 0 == msg_txt.index('/'):
+                    print ("this is a command:::");
+                    result_text = BotResponseView.handle_command(msg_txt[1:].split(' '));
+                payload = {
+                    "method": "sendMessage",
+                    "chat_id": "" + message['chat']['id'],
+                    "text": result_text,
+                };
+            elif 'sticker' in message:
+                payload = {
+                    "method": "sendMessage",
+                    "chat_id": "" + message['message']['chat']['id'],
+                    "sticker": message['sticker']['file_id'],
+                };
+            elif 'photo' in message:
+                payload = {
+                    "method": "sendMessage",
+                    "chat_id": "" + message['message']['chat']['id'],
+                    "photo": message['photo']['file_id'],
+                };
+            else:            
+                payload = {
+                    "method": "sendMessage",
+                    "chat_id": "" + message['chat']['id'],
+                    "text": "Unknowm message type.",
+                };
+            return payload;
+        except Exception as e:
+            print(e)
+            return payload;
+        
+    @staticmethod
+    def handle_command(param):
+        return_text  = "Hey!"
+        command = param.pop(0); 
+        if 'TR' == command:
+            return_text = BotResponseView.translate_to_chinese(' '.join([str(i) for i in param]));
+        else:
+            return_text = BotResponseView.get_bot_response(command);
+        return return_text;
+            
